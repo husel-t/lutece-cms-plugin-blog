fb2c3ebefac366607701418153627713735cc338
TimotheeHrl <tharleaux@gmail.com>
2024-07-09 11:24:52 +0200

    LUT-28232 : Add buttons to archive blog posts

--- src/sql/plugins/blog/upgrade/update_db_blog_3.0.2-3.0.3.sql
+++ src/sql/plugins/blog/upgrade/update_db_blog_3.0.2-3.0.3.sql
@@ -2,3 +2,9 @@ INSERT INTO core_admin_role (role_key, role_description) VALUES('blog_resources'
 INSERT INTO core_admin_role_resource (role_key,resource_type,resource_id,permission) VALUES ('blog_resources','TAG','*','*'),
 INSERT INTO core_admin_role_resource (role_key,resource_type,resource_id,permission) VALUES ('blog_resources','BLOG','*','*');
 INSERT INTO core_user_role (role_key, id_user) VALUES('blog_resources', 1);
+
+
+--
+-- Add new column blog_archive to blog_blog
+--
+ALTER TABLE blog_blog ADD COLUMN is_archived boolean DEFAULT false;
